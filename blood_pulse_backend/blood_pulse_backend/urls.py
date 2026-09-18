2ed2a086b94aa1b43ee751cdd39b4e7d19b7d23c
shawrab27 <nushawrab8.8@gmail.com>
2026-09-19 03:26:47 +0600

    feat(backend): add root index status endpoint

--- blood_pulse_backend/blood_pulse_backend/urls.py
+++ blood_pulse_backend/blood_pulse_backend/urls.py
@@ -16,9 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def root_index(request):
+    return JsonResponse({
+        'name': 'BloodPulse API Server',
+        'status': 'online',
+        'version': '1.0.0',
+        'api_root': '/api/',
+        'admin_dashboard': '/admin/',
+    })
+
 urlpatterns = [
+    path('', root_index, name='root-index'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
